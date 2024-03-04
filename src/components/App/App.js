ca3d4767821bdce6ccb2076f5a21fb7baaa8a012
Casey <arramine@mail.ru>
2024-03-04 13:46:49 +0300

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,17 +4,8 @@ import { useState } from "react";
 import Loader from "../Loader/Loader";
 
 function App() {
-  // const [isLoading, setIsLoading] = useState(false);
-
   return (
     <div className="App">
-      {/* {isLoading ? (
-        <div className="app__loader-container">
-          <Loader />
-        </div>
-      ) : (
-        <OrdersList isLoading={isLoading} setIsLoading={setIsLoading} />
-      )} */}
       <OrdersList/>
     </div>
   );
